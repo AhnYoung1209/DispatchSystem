65833f47c21316465fe9ab88cf27cbbb0241c811
pwwu <1239818707@qq.com>
2015-06-07 19:52:18 +0800

    Updating the .gitignore

--- Main.m
+++ Main.m
@@ -3,7 +3,7 @@ clear; close all; clc; tic;
 % dbstop if error
 TestController = Controller();
 TestTimer = Timer(TestController, 0, 1, 'second');
-for i = 1:1:2000
+for i = 1:1:300
     TestTimer.timepass;
 %     pause(1)
 end 
